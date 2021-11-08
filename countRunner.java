7fce222e8105433744a479d2ad574e8e8a0d1d99
dgoldstein2 <dgoldstein0406@gmail.com>
2021-11-08 05:41:45 -0500

    made sure everything worked

--- countRunner.java
+++ countRunner.java
@@ -2,7 +2,9 @@ public class countRunner {
     public static void main (String[] args){
         countFactors a = new countFactors();
         System.out.println(a.countfactors(25));
-        System.out.print(a.isPrime(37));
+        System.out.println(a.isPrime(37));
+        countPrimes test = new countPrimes();
+        System.out.println(test.countPrimes(24));
 
     }
 }
