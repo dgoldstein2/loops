01c3fea829c5823148fef65e6aea22af63668319
dgoldstein2 <dgoldstein0406@gmail.com>
2021-11-08 17:09:08 -0500

    Update guessingGame.java

--- guessingGame.java
+++ guessingGame.java
@@ -10,21 +10,20 @@ public class guessingGame
   Random randomNumber = new Random();
   Scanner kbd = new Scanner(System.in);
   int computerValue = randomNumber.nextInt(100);
-  int numberOfTries = 0;
   int success = 0;
   int guess = 0;
-  String answer = "y"
+  String answer = "y";
 
 
   
 
 
 
-   while (success == 0 && answer.equals("y"))
+   while (success == 0)
      {
         System.out.println("please enter an integer betwen 1 and 100 inclusive: ");
         guess = kbd.nextInt();
-        numberOfTries++;
+        
 
            if (guess < 1 || guess > 100){
               System.out.println("Invalid input");
@@ -34,6 +33,15 @@ public class guessingGame
            else if (guess == computerValue){
               success++;
               System.out.println("You got it!");
+              System.out.print("Do you want to guess a new number (y/n): ");
+              answer = kbd.nextLine();
+              if (answer.equals("y")){
+               System.out.println("please enter an integer betwen 1 and 100 inclusive: ");
+               guess = kbd.nextInt();
+              }
+              else{
+                 break;
+              }
 
            }
            else if (guess < computerValue){
@@ -42,9 +50,11 @@ public class guessingGame
            else if (guess > computerValue){
               System.out.println("Your guess is too high!");
     }
-        System.out.println("Do you want to guess a new number (y/n)");
-        answer = kbd.nextString();
+    
+        
     }
+    
+      
 
 
 
